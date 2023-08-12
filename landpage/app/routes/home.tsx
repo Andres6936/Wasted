e3dfba461dbf235358a842570debba32750d2a47
Andres6936 <andres6936@gmail.com>
2023-08-12 10:48:47 -0500

    Added Style to Home

--- landpage/app/routes/home.tsx
+++ landpage/app/routes/home.tsx
@@ -23,27 +23,32 @@ export default function Home() {
                 <h1 className={"text-center text-7xl font-medium"}>Join us in the fight for the environment</h1>
             </section>
 
-            <section className={"flex flex-row items-center"}>
-                <div>
-                    <div>
-                        <h6>+685 trees planted</h6>
-                        <p>More of 685 trees were planted last year with the help of our activists</p>
+            <section className={"flex flex-row gap-12 items-center justify-center py-8 px-8"}>
+                <div className={"bg-green-300 w-32 h-32 rounded-3xl"}>
+
+                </div>
+
+                <div className={"flex flex-col w-64 h-48 bg-green-100 rounded-3xl"}>
+                    <div className={"flex-1 flex flex-col justify-center items-center"}>
+                        <h6 className={"font-medium text-lg"}>+685 trees planted</h6>
+                        <p className={"font-light text-center text-xs px-4"}>More of 685 trees were planted last year with the help of our activists</p>
                     </div>
-                    <div>
-                        <h6>6 educational programs</h6>
-                        <p>were launched and developed by our specialists to educate young people</p>
+
+                    <div className={"flex-1 rounded-3xl bg-green-200 flex flex-col justify-center items-center"}>
+                        <h6 className={"font-medium text-lg"}>6 educational programs</h6>
+                        <p className={"font-light text-center text-xs px-4"}>were launched and developed by our specialists to educate young people</p>
                     </div>
                 </div>
 
-                <div>
-                    <p>Share with us your results</p>
+                <div className={"flex bg-green-300 w-48 h-64 rounded-3xl justify-center items-end py-4 px-6"}>
+                    <p className={"font-medium text-lg text-center"}>Share with us your results</p>
                 </div>
 
-                <div>
+                <div className={"bg-green-300 w-64 h-48 rounded-3xl"}>
                     <p>Donate and help the environment</p>
                 </div>
 
-                <div>
+                <div className={"bg-green-300 w-32 h-32 rounded-3xl"}>
                     <p>International environment day</p>
                 </div>
             </section>
