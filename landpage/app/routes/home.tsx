2a6f047356241e237018263fcf3f6a1afbcfd3db
Andres6936 <andres6936@gmail.com>
2023-08-12 14:58:25 -0500

    Fixed Line

--- landpage/app/routes/home.tsx
+++ landpage/app/routes/home.tsx
@@ -63,7 +63,14 @@ export default function Home() {
                 </article>
             </section>
 
-            <section className={"flex flex-row whitespace-nowrap overflow-hidden flex-nowrap gap-8 bg-green-100 py-2"}>
+            <section className={"flex flex-row whitespace-nowrap overflow-hidden flex-nowrap gap-8 bg-green-100 py-2 px-2"}>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
+                <span className={"text-green-950 font-medium"}>Green The Planet</span>
                 <span className={"text-green-950 font-medium"}>Green The Planet</span>
                 <span className={"text-green-950 font-medium"}>Green The Planet</span>
                 <span className={"text-green-950 font-medium"}>Green The Planet</span>
