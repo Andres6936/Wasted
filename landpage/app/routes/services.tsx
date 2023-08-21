23b76a01e9167596c6a83e4d137d25e0256712da
Andres6936 <andres6936@gmail.com>
2023-08-20 20:10:47 -0500

    Added Form

--- landpage/app/routes/services.tsx
+++ landpage/app/routes/services.tsx
@@ -5,10 +5,11 @@ export default function Services() {
   return (
     <>
       <section className={"flex flex-col container mx-auto"}>
-        <HeadNavigator/>
+        <HeadNavigator />
 
         <article className={"flex justify-center items-center py-8"}>
-          <h1 className={"text-center text-6xl md:text-7xl font-medium leading-[1.1] md:leading-none"}>Join us in the <span className={"rounded-3xl bg-green-200 px-1"}>fight</span> for the
+          <h1 className={"text-center text-6xl md:text-7xl font-medium leading-[1.1] md:leading-none"}>Join us in
+            the <span className={"rounded-3xl bg-green-200 px-1"}>fight</span> for the
             environment</h1>
         </article>
 
@@ -45,7 +46,7 @@ export default function Services() {
         </article>
       </section>
 
-      <GreenPlanet/>
+      <GreenPlanet />
 
       <section className={"flex flex-col container mx-auto"}>
         <article className={"border border-gray-500 rounded-2xl mt-12 p-8"}>
@@ -94,6 +95,45 @@ export default function Services() {
           </div>
         </article>
       </section>
+
+      <section className={"flex flex-col container mx-auto"}>
+        <div className={"flex flex-row mt-12 px-8 py-4 bg-green-200 uppercase"}>
+          <h3 className={"flex flex-1 text-lg font-medium"}>Nombre Técnico Asignado</h3>
+          <h3 className={"flex flex-1 text-lg font-medium text-center items-center justify-center"}>
+            Manejo Ambiental</h3>
+          <h3 className={"flex flex-1 text-lg font-medium text-center items-center justify-center"}>
+            Frecuencia 1 Vez al Mes</h3>
+          <h3 className={"flex flex-1 text-lg font-medium text-center items-center justify-center"}>
+            Frecuencia 2 Vez al Mes</h3>
+          <h3 className={"flex flex-1 text-lg font-medium text-center items-center justify-center"}>
+            Frecuencia 4 Vez al Mes</h3>
+        </div>
+
+        <div className={"flex flex-row bg-green-600 text-white px-8 py-4"}>
+          <div className={"flex flex-col flex-1"}>
+            <h2 className={"flex flex-1 font-medium text-2xl"}>Biosanitarios</h2>
+            <h2 className={"flex flex-1 font-medium text-2xl"}>Anatomopatológicos</h2>
+            <h2 className={"flex flex-1 font-medium text-2xl"}>Cortopunzante</h2>
+          </div>
+
+          <div className={"flex flex-col flex-1 text-center"}>
+            <h2 className={"flex flex-1 items-center justify-center font-medium text-2xl"}>Desactivación</h2>
+            <h2 className={"flex flex-[2] items-center justify-center font-medium text-2xl"}>Incineración</h2>
+          </div>
+
+          <div className={"flex flex-col flex-1 text-center"}>
+            <h1 className={"flex flex-1 items-center justify-center text-4xl font-bold px-14"}>$30.000 Hasta 10Kg Mes</h1>
+          </div>
+
+          <div className={"flex flex-col flex-1 text-center"}>
+            <h1 className={"flex flex-1 items-center justify-center text-4xl font-bold px-14"}>$45.000 Hasta 25Kg Mes</h1>
+          </div>
+
+          <div className={"flex flex-col flex-1 text-center"}>
+            <h1 className={"flex flex-1 items-center justify-center text-4xl font-bold px-14"}>$81.000 Hasta 50Kg Mes</h1>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
