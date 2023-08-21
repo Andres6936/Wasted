d195fa4bac14ead83fa1f88ac3087b91990d8539
Andres6936 <andres6936@gmail.com>
2023-08-21 18:32:19 -0500

    Change of Name

--- page/components/services/ObligatoryDocuments.tsx
+++ page/components/services/ObligatoryDocuments.tsx
@@ -5,7 +5,9 @@ export default function() {
     <section className={"flex flex-col container mx-auto"}>
       <article className={"border border-gray-500 rounded-2xl p-8"}>
         <h3 className={"text-2xl font-medium"}>Documentos Requeridos</h3>
-        <p>Documentos requeridos para firma de contrato</p>
+        <p className={"w-[80%]"}>Documentos requeridos para firma de contrato, una vez envié la información necesaria para la cotización,
+          recibirá un corre electrónico con instrucciones para iniciar sesión el sitio web, donde deberá subir los
+          documentos requeridos.</p>
 
         <div className={"flex flex-row gap-8 mt-8"}>
           <div className={"w-[12rem] h-[12rem] bg-green-500 rounded-2xl"}>
@@ -24,5 +26,5 @@ export default function() {
         </div>
       </article>
     </section>
-  )
+  );
 }
--- page/components/services/TypeServiceChoice.tsx
+++ page/components/services/TypeServiceChoice.tsx
@@ -4,7 +4,7 @@ export default function() {
   return (
     <div className={"flex flex-row mt-4"}>
       <div className={"flex flex-col basis-2/5"}>
-        <h5 className={"text-lg font-medium"}>Type of Service</h5>
+        <h5 className={"text-lg font-medium"}>Tipo de Servicio</h5>
         <p className={"text-md"}>Some instructions lorem ipsum is simply dummy text of printing.</p>
       </div>
 
