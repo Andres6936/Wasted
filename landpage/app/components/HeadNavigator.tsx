9f5ecc47194a0d37c77ad04fb28337ce6b1631fe
Andres6936 <andres6936@gmail.com>
2023-08-20 21:40:41 -0500

    Added Links

--- landpage/app/components/HeadNavigator.tsx
+++ landpage/app/components/HeadNavigator.tsx
@@ -1,29 +1,34 @@
+import { Link } from "@remix-run/react";
+
 export default function HeadNavigator() {
   return (
     <article className={"flex flex-row justify-around items-center px-8 py-6"}>
-      <span className={"flex flex-auto font-medium justify-start"}>Wasted</span>
+      <Link to={"/home"} className={"flex flex-auto font-medium justify-start"}>Wasted</Link>
 
       <div className={"hidden md:flex flex-1 flex-row gap-6"}>
-                    <span
-                      className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
-                        Projects
-                    </span>
-        <span
-          className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
-                        Services
-                    </span>
-        <span
-          className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
-                       Campaigns
-                   </span>
-        <span
-          className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
-                       Portal
-                   </span>
+        <Link to={"/home"}
+              className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
+          Projects
+        </Link>
+
+        <Link to={"/services"}
+              className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
+          Services
+        </Link>
+
+        <Link to={"/"}
+              className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
+          Campaigns
+        </Link>
+
+        <Link to={"/login"}
+              className={"flex flex-shrink flex-grow basis-0 rounded-3xl bg-gray-200 py-1 px-3 justify-center text-center"}>
+          Portal
+        </Link>
       </div>
 
       <div className={"flex flex-auto font-medium justify-end"}>
-        <span className={"py-1 px-3 bg-gray-950 text-white rounded-3xl"}>Contact us</span>
+        <Link to={"/contact"} className={"py-1 px-3 bg-gray-950 text-white rounded-3xl"}>Contact us</Link>
       </div>
     </article>
   );
