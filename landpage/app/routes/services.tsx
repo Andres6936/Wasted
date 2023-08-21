dbe51dcff85e62cb39a4ce146a8649b1761080e2
Andres6936 <andres6936@gmail.com>
2023-08-20 21:37:43 -0500

    Added New Section

--- landpage/app/routes/services.tsx
+++ landpage/app/routes/services.tsx
@@ -86,10 +86,41 @@ export default function Services() {
               <p className={"text-md"}>Some instructions lorem ipsum is simply dummy text of printing.</p>
             </div>
 
-            <div className={"flex flex-row basis-3/5"}>
+            <div className={"flex flex-row gap-4 basis-3/5"}>
               <div>
-                <input className={"border border-gray-500 rounded-3xl w-[24rem]"} type="text" />
-                <p>You can't change the street & direction in later use</p>
+                <div className={"flex justify-center items-center w-[3.5rem] h-[3.5rem] bg-green-600 rounded-xl"}>
+                  <span className={"text-3xl text-white font-medium"}>1</span>
+                </div>
+              </div>
+
+              <div>
+                <div className={"flex justify-center items-center w-[3.5rem] h-[3.5rem] bg-green-600 rounded-xl"}>
+                  <span className={"text-3xl text-white font-medium"}>2</span>
+                </div>
+              </div>
+
+              <div>
+                <div className={"flex justify-center items-center w-[3.5rem] h-[3.5rem] bg-green-600 rounded-xl"}>
+                  <span className={"text-3xl text-white font-medium"}>3</span>
+                </div>
+              </div>
+
+              <div>
+                <div className={"flex justify-center items-center w-[3.5rem] h-[3.5rem] bg-green-600 rounded-xl"}>
+                  <span className={"text-3xl text-white font-medium"}>4</span>
+                </div>
+              </div>
+
+              <div>
+                <div className={"flex justify-center items-center w-[3.5rem] h-[3.5rem] bg-green-600 rounded-xl"}>
+                  <span className={"text-3xl text-white font-medium"}>5</span>
+                </div>
+              </div>
+
+              <div>
+                <div className={"flex justify-center items-center w-[3.5rem] h-[3.5rem] bg-green-600 rounded-xl"}>
+                  <span className={"text-3xl text-white font-medium"}>6</span>
+                </div>
               </div>
             </div>
           </div>
